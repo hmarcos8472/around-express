ba22b3b67cf6b5a1f26e88b0adbee0e6b19afd38
Marcos <hz.marcosib@gmail.com>
2020-10-15 14:01:29 -0700

    fixed 200 res from non existant user

--- controllers/cardController.js
+++ controllers/cardController.js
@@ -11,24 +11,38 @@ function getCards(req, res) {
     .catch(() => res.status(500).send({message: "500 Internal server error"}))
 }
 
+/////
 function createCard(req, res) {
   const {name, link } = req.body
   Card.create({name, link, owner: req.user._id})
   .then(card => {res.send({data : card})})
-  .catch(err => {
-    res.status(400).send(err)
+  .catch((err) => {
+    if (err.name === "Validation Error") {
+      res.status(400).send({message : "Card validation failed"})
+    } else {
+      res.status(500).send({message : "Internal server error"})
+    }
   })
 }
 
+/////
 function deleteCard(req, res) {
   const { id } = req.params.cardId;
   return Card.deleteOne(id)
     .then(card => {
       res.status(200).send({card, message: "Card has been deleted"})
     })
-    .catch(() => res.status(500).send({message: "500 Internal server error"}))
+    .catch((err) => {
+     if (err.name = "CastError") {
+       res.status(404).send({message: "The requested card does not exist"});
+     }
+     else {
+       res.status(500).send({message: "500 Internal server error"})
+     }
+   })
 }
 
+/////
 function likeCard(req, res) {
   Card.findByIdAndUpdate(
     req.params.cardId,
@@ -37,6 +51,7 @@ function likeCard(req, res) {
   )
 }
 
+/////
 function dislikeCard(req, res){
   Card.findByIdAndUpdate(
     req.params.cardId,
--- controllers/userController.js
+++ controllers/userController.js
@@ -1,6 +1,7 @@
 const path = require('path')
 const User = require('../models/user.js')
 
+/////
 function getUsers(req, res) {
   return User.find({})
     .then(users => {
@@ -9,17 +10,26 @@ function getUsers(req, res) {
     .catch(() => res.status(500).send({message: "500 Internal server error"}))
 };
 
+/////
 function getSingleUser(req, res) {
   return User.find({_id: req.params.id})
-    .then(user => {
-      if (user) {
+    .then((user) => {
+      if (Object.keys(user).length !== 0) {
         return res.status(200).send(user);
       }
-      res.status(404).send({message: "The requested user does not exist"});
+      return res.status(404).send({message: "User not found"});
     })
-    .catch(() => res.status(500).send({message: "500 Internal server error"}))
+    .catch((err) => {
+     if (err.name === "CastError") {
+       res.status(404).send({message: "User not found"});
+     }
+     else {
+       res.status(500).send({message: "500 Internal server error"})
+     }
+   })
 };
 
+/////
 function createUser(req, res) {
   const {name, about, avatar} = req.body
   return User.countDocuments({})
@@ -29,11 +39,16 @@ function createUser(req, res) {
   .then(user => {
     res.status(200).send(user)
   })
-  .catch(err => {
-    res.status(400).send(err)
+  .catch((err) => {
+    if (err.name === "Validation Error") {
+      res.status(400).send({message : "User validation failed"})
+    } else {
+      res.status(500).send({message : "Internal server error"})
+    }
   })
 }
 
+/////
 function updateUserName(req, res) {
   User.findByIdAndUpdate(req.params.id, "name: req.body")
     .then(user => res.send({ data: req.body }))
@@ -41,6 +56,7 @@ function updateUserName(req, res) {
       res.status(500).send({ message: "500 Internal server error" });})
 };
 
+/////
 function updateAvatar(req, res) {
   User.findByIdAndUpdate(req.params.id, "avatar: req.body")
     .then(user => res.send({ data: req.body }))
